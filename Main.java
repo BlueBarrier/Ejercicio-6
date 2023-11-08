c3905e7121615b95b39db8d5b1c4632c2734e656
Erick Barrera <dannbj1@gmail.com>
2023-11-07 22:14:19 -0600

    Terminación de funcionalidades

--- Main.java
+++ Main.java
@@ -1,30 +1,61 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Operador opps = new Operador();
+        opps.LeerData();
+        ArrayList<Dispositivo> dispositivos = opps.getDispositivos();
         boolean salir = false;
         while (!salir) {
             System.out.println("--Menú--"+
                             "\n 1. Ver Dispositivos"+
-                            "\n 2. Controlar Dispositivo");
-
+                            "\n 2. Controlar Dispositivo"+
+                            "\n 3. Dispositivos más barato"+
+                            "\n 4. Dispositivos más caro");
             try {
                 int opcion = scan.nextInt();
                 switch (opcion) {
                     case 1:
-                        
+                        for (int i = 0; i < dispositivos.size(); i++) {
+                            int indice = i +1;
+                            System.out.println("\n"+indice+". "+dispositivos.get(i).getModelo());
+                        }
                         break;
                     case 2:
-                        opps.controlador(null);
+                        System.out.println("Ingrese el número del dispositivo a controlar: ");
+                        for (int i = 0; i < dispositivos.size(); i++) {
+                            int indice = i +1;
+                            System.out.println("\n"+indice+". "+dispositivos.get(i).getModelo());
+                        }
+                        try {
+                            int dispo = scan.nextInt();
+                            scan.nextLine();
+                            if (!(dispo>=0 || dispo> 14)) {
+                                if (dispositivos.get(dispo-1) instanceof Celular) {
+                                    opps.controlador(dispositivos.get(dispo-1), scan);
+                                }else if (dispositivos.get(dispo-1) instanceof Computadora) {
+                                    opps.controlador(dispositivos.get(dispo-1),scan);
+                                }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case 3:
+                        opps.min();
+                        break;
+                    case 4:
+                        opps.max();
                         break;
                     default:
-
+                        System.out.println("Opción inválida");
                         break;
                 }
             } catch (Exception e) {
                 System.out.println("Entrada inválida");
             }
         }
+        scan.close();
     }
 }
--- Operador.java
+++ Operador.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Operador{
     private ArrayList<Dispositivo> dispositivos = new ArrayList<>();
 
@@ -33,6 +34,10 @@ public class Operador{
         }
     }
 
+    public ArrayList<Dispositivo> getDispositivos() {
+        return dispositivos;
+    }
+
     public Object max(){
         if (dispositivos.isEmpty()) {
             return null;
@@ -60,10 +65,78 @@ public class Operador{
 
     }
 
-    public void controlador(Dispositivo buscar) {
+    public void controlador(Dispositivo buscar, Scanner scan) {
         for (Dispositivo dispositivo : dispositivos) {
             if (dispositivo.equals(buscar)) {
-                
+                System.out.println("Seleccione que desee controlar: ");
+                System.out.println("\n1. bajar volumen"+
+                                    "\n2. subir volumen"+
+                                    "\n3. bajar brillo"+ 
+                                    "\n4. subir brillo"+
+                                    "\n5. información estado"+
+                                    "\n6. información dispositivo"+
+                                    "\n7. cambiar video");
+                try {
+                    int aplicar = scan.nextInt();
+                    scan.nextLine();
+                    if (dispositivo instanceof Celular) {
+                        Celular cel = (Celular) dispositivo;
+                        switch (aplicar) {
+                        case 1:
+                            cel.bajarVolumen();
+                            break;
+                        case 2:
+                            cel.subirVolumen();
+                            break;
+                        case 3:
+                            cel.bajarBrillo();
+                            break;
+                        case 4:
+                            cel.subirBrillo();
+                            break;
+                        case 5:
+                            cel.informacionEstado();
+                            break;
+                        case 6:
+                            cel.informacionDispositivo();
+                            break;
+                        case 7:
+                            // cel.controlarVideos(aplicar, scan);
+                            break;
+                        default:
+                            break;
+                    }
+                    } else if (dispositivo instanceof Computadora) {
+                        Computadora compu = (Computadora) dispositivo;
+                        switch (aplicar) {
+                        case 1:
+                            compu.bajarVolumen();;
+                            break;
+                        case 2:
+                            compu.subirVolumen();
+                            break;
+                        case 3:
+                            compu.bajarBrillo();
+                            break;
+                        case 4:
+                            compu.subirBrillo();                            
+                            break;
+                        case 5:
+                            compu.informacionEstado();
+                            break;
+                        case 6:
+                            compu.informacionDispositivo();
+                            break;
+                        case 7:
+                            // compu.controlarVideos();
+                            break;
+                        default:
+                            break;
+                    }
+                    }                        
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
             }
             
         }
